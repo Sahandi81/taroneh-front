cfb470e5b253e2d46db3c628631cdeea13c3d8e1
hassan asadollahi <hasan.asadolahi2@gmail.com>
2022-11-22 23:52:13 +0330

    taroneh-10-3

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 export const API_URL ='https://api.taroneh.ir';
 
-export const NEXT_URL ='http://127.0.0.1:3001'
+export const NEXT_URL ='http://0.0.0.0:3001'
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = '127.0.0.1'
+const hostname = '0.0.0.0'
 const port =3000
 // process.env.port || 3000
 // when using middleware `hostname` and `port` must be provided below
