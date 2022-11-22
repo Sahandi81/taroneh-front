fd7aea9fbf94809316b8e6d79a42402fb0873dbf
hassan asadollahi <hasan.asadolahi2@gmail.com>
2022-11-22 18:28:39 +0330

    taroneh-5

--- .next/static/chunks/pages/248475.txt-87cee145c001fa90.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[75],{4750:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/248475.txt",function(){return t(4897)}])},4897:function(n,u,t){"use strict";t.r(u);var _=t(5893);u.default=function(){return(0,_.jsx)("div",{})}}},function(n){n.O(0,[774,888,179],(function(){return u=4750,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
