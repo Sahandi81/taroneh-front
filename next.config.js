5de20d6325676e33375369338c2f508c97296a3a
hassan asadollahi <hasan.asadolahi2@gmail.com>
2022-11-22 18:13:34 +0330

    taroneh-3

--- next.config.js
+++ next.config.js
@@ -2,5 +2,10 @@ module.exports = {
   reactStrictMode: true,
   env: {
     MAPBOX_API_key: process.env.MAPBOX_API_key
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
